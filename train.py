88ebf4dd8c1f262d6a8573ce15023fa249452cff
kaiwey2-1561817 <35276469+kaiwey2-1561817@users.noreply.github.com>
2023-03-08 21:00:21 +0800

    add argparser

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ from torch.utils.data import DataLoader
 from torch import optim
 from tqdm import tqdm, trange
 import math
+import argparse
 from sklearn.model_selection import train_test_split
 
 from utils.dataset import MyDataSet, BalancedBatchSampler
@@ -15,6 +16,34 @@ from utils.trainer import train_and_evaluate
 import pdb
 
 random_state = 10
+
+def get_args_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_path",
+                        required=True,
+                        type=str,
+                        help='Path to data to be processed.'
+                        )
+    parser.add_argument("--model_save_name",
+                        required=False,
+                        type=str,
+                        default="clip_test",
+                        help='Name of saved model and results.'
+                        )
+    parser.add_argument("--pretrained_name",
+                        required=False,
+                        type=str,
+                        default='openai/clip-vit-base-patch32',
+                        help='Path to pretrained model.'
+                        )
+    parser.add_argument("--batch_size",
+                        required=False,
+                        type=int,
+                        default=16,
+                        help='Batch size. Default 16. '
+                        )
+    args = parser.parse_args()
+    return args
 
 def finetune(model, train_dataloader, val_dataloader, trainer, model_save_name):
     MODEL_PATH = model_save_name + ".bin"
@@ -72,22 +101,19 @@ def get_train_test_split(df, query2label):
     print(f"val data size: {len(valid)}")
     return train, valid, train_labels, val_labels
 
-def main():
+def main(data_path, model_save_name, pretrained_name='openai/clip-vit-base-patch32', batch_size=16):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
 
-    data_path = Path('/data/kaiwen/ahrefs/dataset')
-    model_save_name = "clip_test"
-    model_path = 'openai/clip-vit-base-patch32'
-    BATCH_SIZE = 16
+    BATCH_SIZE = batch_size
 
     total_df = feather.read_feather(data_path / 'train_final_df.feather')
     query2label = {q: l for l, q in enumerate(total_df['query'].unique())}
     train, valid, train_labels, val_labels = get_train_test_split(total_df, query2label)
 
-    model = CLIPModel.from_pretrained(model_path)
+    model = CLIPModel.from_pretrained(pretrained_name)
     model.to(device)
-    processor = CLIPProcessor.from_pretrained(model_path)
+    processor = CLIPProcessor.from_pretrained(pretrained_name)
 
     train_dataset = MyDataSet(train, processor, query2label)
     train_sampler = BalancedBatchSampler(torch.tensor(train_labels), BATCH_SIZE, 1)
@@ -102,4 +128,12 @@ def main():
     finetune(model, train_dataloader, val_dataloader, trainer, model_save_name)
             
 if __name__=="__main__":
-    main()
+    args = get_args_parser()
+    data_path = args.data_path
+    model_save_name = args.model_save_name
+    pretrained_name = args.pretrained_name
+    batch_size = args.batch_size
+    # data_path = Path('/data/kaiwen/ahrefs/dataset')
+    # model_save_name = "clip_test"
+    # pretrained_name = 'openai/clip-vit-base-patch32'
+    main(data_path, model_save_name, pretrained_name, batch_size)
