7417a95bc8f5f9bbbfaa8b9e486dd38c73cd8dae
kaiwey2-1561817 <35276469+kaiwey2-1561817@users.noreply.github.com>
2023-03-10 15:56:07 +0800

    skip images that cannot be resized

--- inference.py
+++ inference.py
@@ -16,16 +16,20 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 pretrained_name = 'openai/clip-vit-base-patch32'
 model = CLIPModel.from_pretrained(pretrained_name) # load pretrained model
-model.to(device)
+# model.to(device)
 processor = CLIPProcessor.from_pretrained(pretrained_name) # load model processor that help us prepare the data input
 
-test_data = feather.read_feather(data_path / 'test.feather')
+test_data = feather.read_feather(data_path / 'test_final_df.feather')
 submission = pd.read_csv(data_path / 'submission.csv')
 
 predictions = {}
+# images with dimension 1 x 1 x 3 will cause error
+corrupted = []
 
-for i in range(len(test_data)):
+for i in trange(len(test_data)):
     file_id = test_data['id'][i]
+    if file_id in corrupted:
+        continue
     query = "a picture relevant to " + test_data['query'][i]
     folder_num = i // divide_factor
     save_path_final = data_path / f'test_data/{(folder_num+1)*divide_factor}/{file_id}.pkl'
@@ -34,7 +38,11 @@ for i in range(len(test_data)):
             img = pickle.load(f)['image']
         
         image = Image.open(BytesIO(img)).convert("RGB")
-        encoding = processor(text=query, images=image, return_tensors="pt", padding=True)
+        try:
+            encoding = processor(text=query, images=image, return_tensors="pt", padding=True)
+        except ValueError:
+            corrupted.append(file_id)
+            continue
         outputs = model(**encoding)
         text_embed, image_embed = outputs.text_embeds[0], outputs.image_embeds[0]
         dot_product = torch.dot(text_embed, image_embed).item()
@@ -45,5 +53,7 @@ for i in range(len(submission)):
     file_id = submission['id'][i]
     if file_id in predictions:
         submission['is_relevant'][i] = predictions[file_id]
+        
+print(len(corrupted))
         
 submission.to_csv(data_path / 'submission_done.csv')
