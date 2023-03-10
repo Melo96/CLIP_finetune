9e19652cc3fa5a5d8ad111e38f80d9ba07c58eee
kaiwey2-1561817 <35276469+kaiwey2-1561817@users.noreply.github.com>
2023-03-10 22:27:51 +0800

    inference update

--- inference.py
+++ inference.py
@@ -5,6 +5,7 @@ from pyarrow import feather
 from pathlib import Path
 from tqdm import tqdm, trange
 from io import BytesIO
+import PIL
 from PIL import Image
 import pickle
 
@@ -16,10 +17,10 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 pretrained_name = 'openai/clip-vit-base-patch32'
 model = CLIPModel.from_pretrained(pretrained_name) # load pretrained model
-# model.to(device)
+model.to(device)
 processor = CLIPProcessor.from_pretrained(pretrained_name) # load model processor that help us prepare the data input
 
-test_data = feather.read_feather(data_path / 'test_final_df.feather')
+test_data = feather.read_feather(data_path / 'test.feather')
 submission = pd.read_csv(data_path / 'submission.csv')
 
 predictions = {}
@@ -33,18 +34,27 @@ for i in trange(len(test_data)):
         with open(save_path_final, 'rb') as f:
             img = pickle.load(f)['image']
         
-        image = Image.open(BytesIO(img)).convert("RGB").resize((224, 224))
+        try:
+            image = Image.open(BytesIO(img)).convert("RGB").resize((224, 224))
+        # skip corrupted images
+        except PIL.UnidentifiedImageError:
+            continue
+        except OSError:
+            print("Image truncated ", file_id, folder_num)
+            continue
+        
         encoding = processor(text=query, images=image, return_tensors="pt", padding=True)
         
-        outputs = model(**encoding)
+        input_ids=encoding['input_ids'].to(device)
+        attention_mask=encoding['attention_mask'].to(device)
+        pixel_values = encoding['pixel_values'].to(device)
+        outputs = model(pixel_values = pixel_values,input_ids=input_ids, attention_mask=attention_mask)
         text_embed, image_embed = outputs.text_embeds[0], outputs.image_embeds[0]
         dot_product = torch.dot(text_embed, image_embed).item()
-        
-        predictions[file_id] = dot_product
+    
+        submission.loc[i, 'is_relevant'] = dot_product
 
-for i in range(len(submission)):
-    file_id = submission['id'][i]
-    if file_id in predictions:
-        submission['is_relevant'][i] = predictions[file_id]
+print("max similarity: ", max(submission['is_relevant'].values))
+print("min similarity: ", min(submission['is_relevant'].values))
 
 submission.to_csv(data_path / 'submission_done.csv')
